0fd3ab3d2e7ee234135e2fab8715eff8f0e15feb
georgeabdulaziz <george.abdulaziz@ryerson.ca>
2022-04-11 19:52:00 -0400

    minor changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000; //the first one is for heroku 
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 
 io.on('connection', (socket) => {
   socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+    io.emit('chat message', "good job buddy => "+msg);
   });
 });
 
